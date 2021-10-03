e01480245f362fd19dcfe5c755e13a55aff3e8cc
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-10-03 12:17:41 -0400

    Add Activites plugin
    Adds a new plugin called Activites that lets you start Discord Activites
    with a message in a text channel.

---
+++ plugins/Activities/Activities.plugin.js
@@ -0,0 +1,101 @@
+/**
+ * @name Activities
+ * @author Adam Thompson-Sharpe
+ * @description Start Discord Activities with friends.
+ * @version 0.1.0
+ * @authorId 309628148201553920
+ * @source https://github.com/MysteryBlokHed/BetterDiscordPlugins/blob/master/plugins/Activities
+ * @updateUrl https://raw.githubusercontent.com/MysteryBlokHed/BetterDiscordPlugins/master/plugins/Activities/Activities.plugin.js
+ */
+module.exports = class Activities {
+  constructor() {
+    this.activities = {
+      youtube: '880218394199220334',
+      youtubedev: '880218832743055411',
+      poker: '755827207812677713',
+      betrayal: '773336526917861400',
+      fishing: '814288819477020702',
+      chess: '832012774040141894',
+      chessdev: '832012586023256104',
+      lettertile: '879863686565621790',
+      wordsnack: '879863976006127627',
+      doodlecrew: '878067389634314250',
+    }
+  }
+  load() {
+    const MsgHook = window.MsgHook
+    MsgHook.addHook((e) => {
+      var _a
+      const msg = e.hasCommand('.activity')
+      if (msg) {
+        if (!this.activities.hasOwnProperty(msg))
+          return BdApi.alert('Activities', `Unknown activity ${msg}`)
+        // Try to find connected voice channel
+        let channelID
+        const channels = document.querySelector('#channels > div')
+        for (let i = 0; i < channels.children.length; i++) {
+          const el = channels.children[i]
+          // The VC element is 2 children down, so make sure everything exists
+          if (
+            !el.children ||
+            !el.children[0] ||
+            !el.children[0].children ||
+            !el.children[0].children[0]
+          )
+            continue
+          const channel = el.children[0].children[0]
+          for (let j = 0; j < channel.classList.length; j++) {
+            // Get list of classes
+            const className = channel.classList[j]
+            // Current VC will have modeConnected class
+            if (className.includes('modeConnected')) {
+              const thingWithId = channel.children[0].children[0]
+              channelID =
+                (_a = thingWithId.getAttribute('data-list-item-id')) === null ||
+                _a === void 0
+                  ? void 0
+                  : _a.split('___')[1]
+              break
+            }
+          }
+        }
+        // Make sure channel ID was found
+        if (!channelID)
+          return BdApi.alert('Activities', 'Please join a Voice Channel')
+        // Get activity URL
+        fetch(`https://discord.com/api/v8/channels/${channelID}/invites`, {
+          method: 'POST',
+          body: JSON.stringify({
+            max_age: 86400,
+            max_uses: 0,
+            target_application_id: this.activities[msg],
+            target_type: 2,
+            temporary: false,
+            validate: null,
+          }),
+          headers: {
+            Authorization: e.headers.Authorization,
+            'Content-Type': 'application/json',
+          },
+        })
+          .then((res) => res.json())
+          // MsgHook doesn't currently support async, so we can't await the fetch
+          // This means that we'll just need to send a second message with the link
+          .then((invite) => {
+            fetch(e.url, {
+              method: 'POST',
+              body: JSON.stringify({
+                content: `https://discord.gg/${invite.code}`,
+                nonce: `${Math.random() * Math.pow(10, 18)}`,
+                tts: false,
+              }),
+              headers: e.headers,
+            })
+          })
+      }
+    })
+  }
+  start() {}
+  stop() {}
+}
+/// <reference path="../MsgHook/0MsgHook.plugin.ts" />
---
+++ plugins/Activities/README.md
@@ -0,0 +1,35 @@
+# Activities
+
+Start Discord Activities with friends.
+
+## Use
+
+This requires the [MsgHook Plugin] to work.
+To use it, join a voice channel and type `.activity <game>` in any text channel.
+You'll send a link that can be used to start an activity.
+Make sure to click the blue part of the link and not the "Play" button.
+
+![Demo](img/demo.gif)
+
+## Available Activities
+
+| Activity              | Message                |
+| :-------------------- | :--------------------- |
+| YouTube               | `.activity youtube`    |
+| YouTube (Development) | `.activity youtubedev` |
+| Poker                 | `.activity poker`      |
+| Betrayal.io           | `.activity betrayal`   |
+| Fishing               | `.activity fishing`    |
+| Chess                 | `.activity chess`      |
+| Chess (Development)   | `.activity chessdev`   |
+| Letter Tile           | `.activity lettertile` |
+| Word Snack            | `.activity wordsnack`  |
+| Doodle Crew           | `.activity doodlecrew` |
+
+## Credits
+
+The table of application ID's was taken from [discord-together] by RemyK888.
+You can view its license [here](https://github.com/RemyK888/discord-together/blob/main/LICENSE).
+
+[msghook plugin]: https://github.com/MysteryBlokHed/BetterDiscordPlugins/blob/master/plugins/MsgHook
+[discord-together]: https://github.com/RemyK888/discord-together/blob/main/src/DiscordTogether.js
