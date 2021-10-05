1087140d3cd8338ba18fc2112383f0e9bb7cfb17
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-10-04 20:26:11 -0400

    Update version and update CHANGELOG.md

--- plugins/MsgHook/0MsgHook.plugin.js
+++ plugins/MsgHook/0MsgHook.plugin.js
@@ -2,7 +2,7 @@
  * @name MsgHook
  * @author Adam Thompson-Sharpe
  * @description Run code when messages are sent or edited.
- * @version 0.4.0
+ * @version 0.4.1
  * @authorId 309628148201553920
  * @source https://github.com/MysteryBlokHed/BetterDiscordPlugins/blob/master/plugins/MsgHook
  * @updateUrl https://raw.githubusercontent.com/MysteryBlokHed/BetterDiscordPlugins/master/plugins/MsgHook/MsgHook.plugin.js
@@ -26,7 +26,7 @@ module.exports = class MsgHook {
     // Add MsgHook object to window
     window.MsgHook = {
       enabled: false,
-      version: '0.4.0',
+      version: '0.4.1',
       addHook: (hook) => {
         let id = 0
         // Generate random ID's until we get one that isn't taken
--- plugins/MsgHook/CHANGELOG.md
+++ plugins/MsgHook/CHANGELOG.md
@@ -7,6 +7,12 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 Changelogs are different for each plugin.
 
+## 0.4.1
+
+### Changed
+
+- Updated TypeScript-related docs
+
 ## 0.4.0
 
 ### Added
