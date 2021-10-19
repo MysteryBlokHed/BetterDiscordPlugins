27ab07bb26540882032738a05bc0b0d75baa2f24
Adam Thompson-Sharpe <adamthompsonsharpe@gmail.com>
2021-10-19 15:38:37 -0400

    Add Spammer plugin (#9)
    * Add Spammer plugin
    
    * Change `setInterval` to `window.setInterval`
    
    Prevents TypeScript from confusing Node's own setInterval function and
    the normal setInterval function.
    
    * Update nonce generation
    
    * Update RegEx

---
+++ plugins/Spammer/Spammer.plugin.js
@@ -0,0 +1,53 @@
+/**
+ * @name Spammer
+ * @author Adam Thompson-Sharpe
+ * @description Spam messages in a Discord channel.
+ * @version 0.1.0
+ * @authorId 309628148201553920
+ * @source https://github.com/MysteryBlokHed/BetterDiscordPlugins/blob/master/plugins/Spammer
+ * @updateUrl https://raw.githubusercontent.com/MysteryBlokHed/BetterDiscordPlugins/master/plugins/Spammer/Spammer.plugin.js
+ */
+module.exports = class Spammer {
+  constructor() {
+    this.spamIntervals = []
+    this.hookID = 0
+  }
+  checkVersion(current, minimum) {
+    const currentMinor = parseInt(current.split('.')[1])
+    const minimumMinor = parseInt(minimum.split('.')[1])
+    return currentMinor >= minimumMinor
+  }
+  start() {
+    if (!window.MsgHook || !this.checkVersion(window.MsgHook.version, '0.4.0'))
+      return
+    const command = /^\.spam\s*\(\s*(?:"(.*)")\s*(?:,\s*(\d+)\s*)?,?\s*\)$/i
+    this.hookID = window.MsgHook.addHook(e => {
+      const match = command.exec(e.msg)
+      if (match) {
+        const message = match[1]
+        const interval = parseInt(match[2] ?? 1500)
+        this.spamIntervals.push(
+          window.setInterval(() => {
+            fetch(e.url, {
+              method: 'POST',
+              headers: e.headers,
+              body: JSON.stringify({
+                content: message,
+                nonce: Date.now() * 4194304,
+                tts: false,
+              }),
+            })
+          }, interval)
+        )
+        return message
+      } else return
+    })
+  }
+  stop() {
+    if (!window.MsgHook || !this.checkVersion(window.MsgHook.version, '0.4.0'))
+      return
+    for (const interval of this.spamIntervals) clearInterval(interval)
+    window.MsgHook.removeHook(this.hookID)
+  }
+}
+/// <reference types="../MsgHook/0MsgHook.plugin.ts" />
